8e14341ed88a8bce9bd23a1abf73ff90136bfff4
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-09-05 12:02:15 +0700

    Angular Upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ImagesComponent } from './images/images.component';
+import { ImgComponent } from './images/img/img.component';
+import { ListComponent } from './images/list/list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'image/upload', pathMatch: 'full'},
+  { path: 'image', component: ImagesComponent,
+    children: [
+      { path: 'upload', component: ImgComponent },
+      { path: 'list',   component: ListComponent},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImagesComponent } from './images/images.component';
+import { ImgComponent } from './images/img/img.component';
+import { ListComponent } from './images/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImagesComponent,
+    ImgComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule,
